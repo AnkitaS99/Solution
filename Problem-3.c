3e047b655685040fb0a0db2ceb38f46072543951
Ankita Singh <133628334+AnkitaS99@users.noreply.github.com>
2025-12-10 13:39:06 +0530

    Create Problem-3.c

---
+++ Problem-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a,i;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    for (i = 1; i <= a; i += 2) 
+    {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
